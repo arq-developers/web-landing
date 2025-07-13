224b953a9feefad2a405e813fa18ba1168df7d9e
Luisa Eustaquio <95476379+xmuwisa@users.noreply.github.com>
2025-07-13 11:24:28 +0800

    feat/ARQ-11: added how arq works section, modified its previous sections

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,14 +2,12 @@ import Link from "next/link";
 import React from "react";
 
 import { Hero, Goals, WhatIsARQ, HowARQWorks } from "@/components/landing";
-import WhyItMatters from "@/components/shared/matters";
 
 export default function Page() {
 	return (
 		<div>
 			<Hero />
 			<Goals />
-			<WhyItMatters />
 			<WhatIsARQ />
 			<HowARQWorks />
 		</div>
